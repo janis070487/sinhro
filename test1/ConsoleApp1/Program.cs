04045e705d3d224a1dda03dfab9a68aa2db4c184
janis070487 <janis070487@gmail.com>
2021-04-05 16:54:57 +0300

    ok

---
+++ test1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+namespace uzdevums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberFormatInfo nfi = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ",",
+            };
+            string a;
+            string b;
+
+            Console.WriteLine("Ievadi skaitli 1");
+            a = Console.ReadLine();
+            Console.WriteLine("Ievadi skaitli 2");
+            b = Console.ReadLine();
+
+            try
+            {
+                double aa = double.Parse(a, nfi);
+                double bb = double.Parse(b, nfi);
+                double sum = (aa + bb) / 2;
+
+                Console.WriteLine("Vidējais aritmētiskais = " + sum);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ievadīts nekorekts skaitlis!!!");
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+s
---
+++ test1/variants2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uzdevums1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            string b;
+            Console.WriteLine("Ievadi skaitli 1");
+            a = Console.ReadLine();
+            Console.WriteLine("Ievadi skaitli 2");
+            b = Console.ReadLine();
+
+            double aa = Convert.ToDouble(a);
+            double bb = Convert.ToDouble(b);
+            double sum = (aa + bb) / 2;
+
+            Console.WriteLine("Vidējais aritmētiskais = " + sum);
+
+            Console.ReadLine();
+        }
+    }
+}
